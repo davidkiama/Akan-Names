a736f79b5ef12d06754a608c69e627717fd92efc
davidkiama <davidkiama98@gmail.com>
2021-12-10 12:49:11 +0300

    Added a notification banner for testing purposes

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 "use strict";
+
+const banner = document.querySelector(".banner");
+const date = document.querySelector(".form__input");
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  banner.textContent = date.value;
+});
